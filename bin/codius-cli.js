8bd1e8137a23a22ea938abde0d5a30d04c127036
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-07-30 17:35:40 -0700

    [FEATURE] Use nconf for better option parsing

--- bin/codius-cli.js
+++ bin/codius-cli.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
-var CommandParser = require('../lib/commandparser').CommandParser;
+var nconf  = require('nconf');
 var Codius = require('../lib/codius').Codius;
+var Config = require('../lib/config').Config;
 
-var parser = new CommandParser();
-
-parser.parseEnv(process.env);
-parser.parseArgv(process.argv);
+// Use nconf to load command line arguments and environment variables
+nconf.argv()
+     .env()
+     .file({ file: 'HOME/.config/codius/cli.json' });
+var config = new Config(nconf.get());
+var command = nconf.get('_')[0];
 
 var codius = new Codius();
-codius.load(parser.getConfig()).then(function () {
-  codius.runCommand(codius.config.command);
+codius.load(config).then(function () {
+  codius.runCommand(command);
 }).catch(function (err) {
   console.error(err.message);
   process.exit(1);
