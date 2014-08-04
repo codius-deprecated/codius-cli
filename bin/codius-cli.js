87e659be0b8bfb679d7a83e34ceb3b1db3f1791c
Stefan Thomas <justmoon@members.fsf.org>
2014-08-03 22:03:47 -0700

    [FEATURE] Winston for logging.

--- bin/codius-cli.js
+++ bin/codius-cli.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
-var nconf  = require('nconf');
+var winston = require('winston');
+var nconf = require('nconf');
 var Codius = require('../lib/codius').Codius;
 var Config = require('../lib/config').Config;
+
+winston.cli();
 
 // Use nconf to load command line arguments and environment variables
 nconf.argv()
@@ -10,6 +13,10 @@ nconf.argv()
      .file({ file: 'HOME/.config/codius/cli.json' });
 var config = new Config(nconf.get());
 var command = nconf.get('_')[0];
+
+if (config.config.debug) {
+  winston.default.transports.console.level = 'debug';
+}
 
 var codius = new Codius();
 codius.load(config).then(function () {
